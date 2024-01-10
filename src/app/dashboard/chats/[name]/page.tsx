ac0b74727b801bdaa59f89b0d6c9837e5abf779b
pmgzo <pierremickael.gonzalo@gmail.com>
2024-01-10 10:07:08 +0100

    feat(error-handling): error handling with suspense balises

--- src/app/dashboard/chats/[name]/page.tsx
+++ src/app/dashboard/chats/[name]/page.tsx
@@ -4,6 +4,7 @@ import Loading from '@/app/loading';
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 import Conversation from './conversation';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export default function Page({ params }: { params: { name: string } }) {
 	const searchParams = useSearchParams();
@@ -15,12 +16,14 @@ export default function Page({ params }: { params: { name: string } }) {
 			<div className="text-xl flex justify-center w-5/6 my-5">
 				{params.name}
 			</div>
-			<Suspense fallback={<Loading />}>
-				<Conversation
-					conversationId={Number(conversationId)}
-					peerId={Number(peerId)}
-				/>
-			</Suspense>
+			<ErrorBoundary fallback={<div>Couldn't load conversation</div>}>
+				<Suspense fallback={<Loading />}>
+					<Conversation
+						conversationId={Number(conversationId)}
+						peerId={Number(peerId)}
+					/>
+				</Suspense>
+			</ErrorBoundary>
 		</div>
 	);
 }
